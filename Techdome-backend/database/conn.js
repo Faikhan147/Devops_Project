80c8175187b2608180fb20b68144fc550cd02169
Faisal Khan <faisalkhanattari35@gmail.com>
2024-12-08 17:27:21 +0530

    Delete Techdome-backend directory

--- Techdome-backend/database/conn.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.DB).then(() => {
-    console.log("connection established...!");
-}).catch((error) => {
-    console.log(error);
-})
--- Techdome-backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { register, login, getUserData } = require("../controller/userController");
-
-router.post("/register", register);
-router.post("/login", login);
-router.post("/get_user_data", getUserData);
-
-module.exports = router;
